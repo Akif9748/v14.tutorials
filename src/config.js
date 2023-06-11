2311a51539ba284f2d5cb802c2c5437ff018a6bf
Pudi <63150613+pudidev@users.noreply.github.com>
2023-06-11 19:02:57 +0300

    Bölüm #2

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: "!",
-    token: "MTExNzAzODEzNTYwMDIzMDQxMA.GZ7ggk.NxTwfUNnXf9hlTbwyzXvwGvyPwoTqJV52u4Mzw",
-    mongo: "mongodb+srv://pudimongo:FD4nYAnvi6uP6jiC@cluster0.a7cjrcj.mongodb.net/data?retryWrites=true&w=majority"
+    token: "TOKEN",
+    mongo: "MONGODB"
 }
