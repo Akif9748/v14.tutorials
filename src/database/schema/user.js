acb0b8d1e33f1bdf7458405f584cea6914925b29
Akif9748 <akif9748@gmail.com>
2023-06-12 10:23:08 +0300

    default date sorunu çözüldü İşaretleyici sayıdan tarihe geçirildi, ayrıca bot ilk açıldığındaki tarihi vermek yerine,  artık kullanıcının kaydedildiği tarih veriliyor

--- src/database/schema/user.js
+++ src/database/schema/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model("User", new mongoose.Schema({
-    id: { type: String},
-    registeredAt: { type: Number, default: Date.now() },
+    id: String,
+    registeredAt: { type: Date, default: Date.now },
 }));
