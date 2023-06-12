0fe2202f5b6e37b7cafc26c2dad87742e5b84732
Akif9748 <akif9748@gmail.com>
2023-06-12 10:24:28 +0300

    Video'daki olay gerçek manada yapıldı Videoda eğer yoksa oluştursun denmiş, burda o olmuyordu, düzeltildi. https://youtu.be/U0kb5NPKXlQ?t=585

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,18 +1,3 @@
-const Discord = require('discord.js');
-const config = require('.././config');
 const userSchema = require('../database/schema/user.js');
 
-module.exports.fetchUser = async function(key) {
-
-    let userDB = await userSchema.findOne({ id: key })
-    if(!userDB) {
-        return userDB;
-    }else{
-        userDB = new userSchema({
-            id: key,
-            registeredAt: Date.now()
-    })
-    await userDB.save().catch(err => console.log(err))
-    return userDB;
-    }
-};
+module.exports.fetchUser = async id => await userSchema.findOne({ id }) || await userSchema.create({ id });
