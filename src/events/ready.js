0c2ccf6379a416e3f563c798bb15dca364e76222
Akif9748 <akif9748@gmail.com>
2023-06-12 15:37:15 +0300

    index.js düzeltildi Momente gerek yok, daha türkçeye yatkın iç js modulu var ASYNC olmasına gerek yok requirelerin process eventleri daha iyi loglansın Boş yere array açmaya gerek yok komutlar için, ayrıca ready için 2. bir dinleyiciye gerek yok, tek yerde biter.

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,23 @@
-const { ActivityType } = require("discord.js")
+const { ActivityType } = require("discord.js");
+const { REST } = require('@discordjs/rest');
+
 module.exports = {
 	name: 'ready',
 	once: true,
-	execute(client) {
-    let activities = [ `merhaba`, `${client.user.username}` ], i = 0;
-    setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
-}};
+	async execute(client) {
+		let activities = [`merhaba`, `${client.user.username}`], i = 0;
+
+		setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 22000);
+		try {
+			const rest = new REST({ version: '10' }).setToken(client.token);
+			await rest.put(
+				`/applications/${client.user.id}/commands`, { body: client.slashcommands.toJSON().map(cmd => cmd.data) }
+			);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			client.log('Komutlar aktif!');
+		}
+	
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], 
-  partials: [Partials.Message, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]});
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent],
+  partials: [Partials.Message, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember]
+});
 const config = require("../config.json");
 const { readdirSync } = require("fs")
-const moment = require("moment");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v10');
 const mongoose = require('mongoose');
 
 let token = config.token
@@ -15,76 +14,38 @@ client.slashcommands = new Collection()
 client.commandaliases = new Collection()
 client.Database = require('./database/mongodb.js')
 
+const log = x => console.log(`[${new Date().toLocaleString()}]`, x);
+client.log = log;
 mongoose.connect(config.mongo, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
+}).then(() =>
   log('MongoDB Connected.')
-}).catch((err) => {
-  log(err)
-})
-const rest = new REST({ version: '10' }).setToken(token);
+).catch(log)
 
-const log = x => { console.log(`[${moment().format("DD-MM-YYYY HH:mm:ss")}] ${x}`) };
-
-const commands = []
-readdirSync('./src/commands/normal').forEach(async file => {
-  const command = await require(`./commands/normal/${file}`);
-  if(command) {
+readdirSync('./src/commands/normal').forEach(file => {
+  const command = require(`./commands/normal/${file}`);
+  if (command) {
     client.commands.set(command.name, command)
-    commands.push(command.name, command);
-    if(command.aliases && Array.isArray(command.aliases)) {
-       command.aliases.forEach(alias => {
-        client.commandaliases.set(alias, command.name)  
-})
-}}})
+    if (command.aliases && Array.isArray(command.aliases))
+      command.aliases.forEach(alias => client.commandaliases.set(alias, command.name))
+  }
+});
 
-const slashcommands = [];
-readdirSync('./src/commands/slash').forEach(async file => {
-  const command = await require(`./commands/slash/${file}`);
-  slashcommands.push(command.data.toJSON());
+readdirSync('./src/commands/slash').forEach(file => {
+  const command = require(`./commands/slash/${file}`);
   client.slashcommands.set(command.data.name, command);
+});
+
+readdirSync('./src/events').forEach(file => {
+  const event = require(`./events/${file}`);
+  client[event.once ? "once" : "on"](event.name, (...args) => event.execute(...args));
 })
 
+process.on("unhandledRejection", log)
+process.on("uncaughtException", log)
+process.on("uncaughtExceptionMonitor", log)
 
-
-
-
-client.on("ready", async () => {
-        try {
-            await rest.put(
-                Routes.applicationCommands(client.user.id),
-                { body: slashcommands },
-            );
-        } catch (error) {
-            console.error(error);
-        }
-    log(`${client.user.username} Aktif Edildi!`);
-})
-
-
-readdirSync('./src/events').forEach(async file => {
-	const event = await require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-})
-
-
-process.on("unhandledRejection", e => { 
-   console.log(e)
- }) 
-process.on("uncaughtException", e => { 
-   console.log(e)
- })  
-process.on("uncaughtExceptionMonitor", e => { 
-   console.log(e)
- })
-
-client.login(token)
-
-
+client.login(token);
 
 require("./express.js");
